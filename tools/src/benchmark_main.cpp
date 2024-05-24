3beadcf6e02e038d886b458f42387a7ee9dcd553
Tim Niederhausen <tim@rnc-ag.de>
2024-05-24 22:19:09 +0200

    feat: Add benchmark tool boilerplate

---
+++ tools/src/benchmark_main.cpp
@@ -0,0 +1,15 @@
+#include <benchmark/benchmark.h>
+
+// Including 'hpx/hpx_main.hpp' instead of the usual 'hpx/hpx_init.hpp' enables
+// to use the plain C-main below as the direct main HPX entry point.
+#include <hpx/hpx_main.hpp>
+
+int main(int argc, char** argv)
+{
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv))
+    return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+  return 0;
+}
